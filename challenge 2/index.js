714a9a3d8b419da7d869a0819cf9a2ba6d27f6d7
aryans31 <aryan.saxena3105@gmail.com>
2021-07-22 11:53:03 +0530

    challenge 2 completed

---
+++ challenge 2/index.js
@@ -0,0 +1,10 @@
+console.log("chalenge 2 image generation")
+
+function generate()
+{
+    var image = document.createElement('img');
+    var div = document.getElementById('img-gen');
+    image.src= "4.jpg";
+    div.appendChild(image);
+
+}  
